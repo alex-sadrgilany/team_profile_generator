37664b3348f3f347e6137e5d7cd737332d70f853
AS <asadrgilany7@gmail.com>
2021-10-21 13:37:50 -0400

    broke down inquirer prompts properly

--- index.js
+++ index.js
@@ -1,49 +1,150 @@
 const inquirer = require("inquirer");
 const {writeFile} = require("./utils/generateHTML");
 const generateIndexHTML = require("./src/htmlTemplate");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const questions = [
+const teamArray = [];
+
+const questionsManager = [
     {
         type: "input",
-        name: "nameM",
+        name: "nameManager",
         message: "What is the Manager's name?"
     },
     {
         type: "input",
-        name: "idM",
+        name: "idManager",
         message: "What is the Manager's employee ID?"
     },
     {
         type: "input",
-        name: "emailM",
+        name: "emailManager",
         message: "What is the Manager's email address?"
     },
     {
         type: "input",
-        name: "officeNumber",
+        name: "officeManager",
         message: "What is the Manager's office number?"
+    }
+];
+
+const questionsEngineer = [
+    {
+        type: "input",
+        name: "nameEngineer",
+        message: "What is the Engineer's name?"
     },
     {
-        type: "list",
-        name: "continue",
-        message: "Please select an option to continue",
+        type: "input",
+        name: "idEngineer",
+        message: "What is the Engineer's employee ID?"
+    },
+    {
+        type: "input",
+        name: "emailEngineer",
+        message: "What is the Engineer's email address?"
+    },
+    {
+        type: "input",
+        name: "githubEngineer",
+        message: "What is the Engineer's GitHub username?"
+    }
+];
+
+const questionsIntern = [
+    {
+        type: "input",
+        name: "nameIntern",
+        message: "What is the Intern's name?"
+    },
+    {
+        type: "input",
+        name: "idIntern",
+        message: "What is the Intern's employee ID?"
+    },
+    {
+        type: "input",
+        name: "emailIntern",
+        message: "What is the Intern's email address?"
+    },
+    {
+        type: "input",
+        name: "schoolIntern",
+        message: "What school is the Intern attending?"
+    }
+];
+
+const questionsNewMember = [
+    {
+        type: "rawlist",
+        name: "newMember",
+        message: "Please select a new member to add to your team.",
         choices: [
             "Add an Engineer",
             "Add an Intern",
-            "Finish Team"
+            "I'm finished building my Team Profile"
         ]
     }
 ];
 
-const promptUser = () => {
-    return inquirer.prompt(questions);
+const promptUserManager = () => {
+    inquirer.prompt(questionsManager)
+    .then(answers => {
+        const manager = new Manager(answers.nameManager, answers.idManager, answers.emailManager, answers.officeManager);
+        teamArray.push(manager);
+        addMember();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+const promptUserEngineer = () => {
+    inquirer.prompt(questionsEngineer)
+    .then(answers => {
+        const engineer = new Engineer(answers.nameEngineer, answers.idEngineer, answers.emailEngineer, answers.githubEngineer);
+        teamArray.push(engineer);
+        addMember();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+const promptUserIntern = () => {
+    inquirer.prompt(questionsIntern)
+    .then(answers => {
+        const intern = new Intern(answers.nameIntern, answers.idIntern, answers.emailIntern, answers.schoolIntern);
+        teamArray.push(intern);
+        addMember();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+const addMember = () => {
+    inquirer.prompt(questionsNewMember)
+    .then(answers => {
+        switch(answers.newMember) {
+            case "Add an Engineer":
+                promptUserEngineer();
+                break;
+            case "Add an Intern":
+                promptUserIntern();
+                break;
+            case "I'm finished building my Team Profile":
+                console.log(teamArray);
+                init();
+                break;
+        }
+    })
 }
 
 const init = () => {
-    promptUser()
-    .then(answers => {
-        return generateIndexHTML(answers);
-    })
+    generateIndexHTML(teamArray)
     .then(html => {
         return writeFile(html);
     })
@@ -55,4 +156,4 @@ const init = () => {
     });
 };
 
-init();
+promptUserManager();
